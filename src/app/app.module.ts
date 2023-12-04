54c45fa97332743ee02a9425f0d7ce05fb5a8369
bitxuji <bitxuji@gmail.com>
2023-12-04 22:48:56 +0900

    The setup code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VoterComponent } from './1-voter/voter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoterComponent
   ],
   imports: [
     BrowserModule,
