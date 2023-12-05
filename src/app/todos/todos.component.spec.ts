c7d61362eadf3abe008f2887b652dff929c7956d
bitxuji <bitxuji@gmail.com>
2023-12-05 22:20:19 +0900

    Getting the dependencies

--- src/app/todos/todos.component.spec.ts
+++ src/app/todos/todos.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
 import { HttpClientModule } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -16,10 +17,16 @@ describe('TodosComponent', () => {
     });
     fixture = TestBed.createComponent(TodosComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should load todos from the server', () => {
+    let service = TestBed.inject(TodoService);
+    // let service = fixture.debugElement.injector.get(TodoService);
+    spyOn(service, 'getTodos').and.returnValue(of([1, 2, 3]));
+
+    fixture.detectChanges();
+
+    expect(component.todos.length).toBe(3);
+
   });
 });
