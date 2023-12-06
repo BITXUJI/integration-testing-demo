dfdc9d9c4d8acdef33f1b2131fe77cae76f00998
bitxuji <bitxuji@gmail.com>
2023-12-06 23:23:05 +0900

    Dealing with route params

--- src/app/user-details/user-details.component.spec.ts
+++ src/app/user-details/user-details.component.spec.ts
@@ -1,16 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserDetailsComponent } from './user-details.component';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 
 class RouterStub {
   navigate(params: any) { }
 }
+// class ActivatedRouteStub {
+//   params: Observable<any> = of({});
+// }
 class ActivatedRouteStub {
-  params: Observable<any> = of();
-
+  private subject = new BehaviorSubject({});  // import BehaviorSubject from 'rxjs'
+  push(value: any) {
+    this.subject.next(value);
+  }
+  get params() {
+    return this.subject.asObservable();
+  }
 }
+
+
+
 describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
@@ -25,15 +36,28 @@ describe('UserDetailsComponent', () => {
     });
     fixture = TestBed.createComponent(UserDetailsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
   });
 
   it('should redirect the user to the users page after saving', () => {
+    fixture.detectChanges();
     let router = TestBed.inject(Router);
     let spy = spyOn(router, 'navigate');
 
     component.save();
 
     expect(spy).toHaveBeenCalledWith(['users']);
+  });
+  it('should navigate the user to the not found page when an invalid user id is passed', () => {
+    let router = TestBed.inject(Router);
+    let spy = spyOn(router, 'navigate');
+
+    // let route: ActivatedRouteStub = TestBed.inject(ActivatedRoute);
+    // route.params = of({ id: 0 });
+    let route = TestBed.inject(ActivatedRoute) as any as ActivatedRouteStub;
+    route.push({ id: 0 });
+    fixture.detectChanges();
+
+    expect(spy).toHaveBeenCalledWith(['not-found']);
   });
 });
